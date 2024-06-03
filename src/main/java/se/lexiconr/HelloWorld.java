6f565712c9af728e13b67a016c888a5751f0ac7f
Alexander-Persson2 <alexander.persson77@gmail.com>
2024-06-03 09:43:36 +0200

    First init with java demo project

---
+++ src/main/java/se/lexiconr/HelloWorld.java
@@ -0,0 +1,19 @@
+package se.lexiconr;
+
+/**
+ * Hello world!
+ */
+   // this is a single-line comment
+
+   /*
+   this is a multi-line comment
+   it can span multiple lines
+    */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        // java statement,
+        System.out.println("Hello World!");
+    }
+}
